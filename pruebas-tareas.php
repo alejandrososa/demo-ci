b7a4d74ec639b82c52c52cca0fe6e1c76cc755d9
Alex Sosa <elpastorsalomon@gmail.com>
2017-01-01 19:23:42 +0100

    Modificación desde la rama develop

--- pruebas-tareas.php
+++ pruebas-tareas.php
@@ -5,7 +5,7 @@
      * Fecha: 1/1/17 - 18:12
      */
 
-    $numeros = [1,2,3,4,5,7];
+    $numeros = [1,2,3,4,5,7,8,9,10];
 
     foreach ($numeros as $index => $numero) {
         echo "Posición $index = $numero \n";
