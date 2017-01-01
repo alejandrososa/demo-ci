57ca64f1ae1d9813b839982c5c4608f30022feec
Alex Sosa <elpastorsalomon@gmail.com>
2017-01-01 18:28:27 +0100

    Primer commit para el demo

---
+++ pruebas-tareas.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Creado para Sites.
+     * Desarrollador: Alejandro Sosa <alesjohnson@hotmail.com>
+     * Fecha: 1/1/17 - 18:12
+     */
+
+    $numeros = [1,2,3,4,5,6,7];
+
+    foreach ($numeros as $index => $numero) {
+        echo "Posición $index = $numero \n";
+    }
