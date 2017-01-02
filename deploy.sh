9e66e5844e205d73e8b8f72bd75a3d80a9bda8ea
Alejandro Sosa <alejandro.sosa.mediaadgo@gmail.com>
2017-01-02 16:17:46 +0100

    Fichero index htaccess

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,62 @@
-#!/bin/sh
-whoami
+#!/bin/bash
+#pwd
+#whoami
+#echo $1
+#echo $2
+#echo "`date` Usuario `whoami` inicio el script, opciones: rama $1 en el entorno $2."'\r' >> resultado.log
 
-pwd
-whoami
-echo $1
-echo $2
-echo "`date` Usuario `whoami` inicio el script, opciones: rama $1 en el entorno $2."'\r' >> resultado.log
+#constantes
+RAMA_ROLLBACK="rollback"
+RAMA_DEVELOP="develop"
+RAMA_MASTER="master"
+ENTORNO_PRE="pre"
+ENTORNO_PRO="pro"
+RUTA_ENTORNO_PRE="/var/www/html/demo-ci-pre/"
+RUTA_ENTORNO_PRO="/var/www/html/demo-ci-pro/"
+
+#parametros entrantes
+inputRama=$1
+inputEntorno=$2
+
+#metodos
+
+# Despliega version en entorno pre
+# @param $inputRama string
+despliegueVersion () {
+    rama=$1
+    rutaProyecto=$2
+    mensaje=$3
+
+    if [ -z $mensaje ]; then
+        mensaje="\033[92mOK. Se ha desplegado $rama en $ENTORNO_PRE"
+    fi
+
+    if [ ! -z "$rama" ]; then
+        cd $rutaProyecto
+        git fetch --all
+        git reset --hard origin/$rama
+        git pull origin $rama
+        echo $mensaje
+    fi
+}
+
+
+
+selectEntorno () {
+    case $2 in
+      $ENTORNO_PRE)
+         despliegueVersion $inputRama $RUTA_ENTORNO_PRE
+      ;;
+      $ENTORNO_PRO)
+         despliegueVersion $inputRama $RUTA_ENTORNO_PRO
+      ;;
+      *)
+         echo "Opciones invalidas $1 $2:"
+      ;;
+    esac
+}
+
+
+
+#despliegue en entornos
+selectEntorno $inputRama $inputEntorno
